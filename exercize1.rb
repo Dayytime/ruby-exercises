f46b54c1798f96c966596476b2186e535e59911d
Majd <93227924+Dayytime@users.noreply.github.com>
2021-10-27 18:14:35 -0400

    added file

---
+++ exercize1.rb
@@ -0,0 +1,11 @@
+puts "Hours in a year"
+puts 365 * 24 
+puts ""
+puts "Minutes in a Decade"
+puts (365 * 10) * 24 * 60
+puts ""
+puts "Seconds I have lived"
+puts (365 * 15) * 24 * 60 * 60
+puts ""
+puts "Author age"
+puts 1390000000/(365 * 24 * 60 * 60)
