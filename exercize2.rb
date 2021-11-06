b67d968c43d090b1bad3792eea72379be44ef524
Majd <93227924+Dayytime@users.noreply.github.com>
2021-11-06 19:25:44 -0400

    stuff
    thing

---
+++ exercize2.rb
@@ -0,0 +1,16 @@
+
+puts "What's your first name?"
+f_name = gets.chomp
+puts "What's your middle name?"
+m_name = gets.chomp
+puts "What's your last name"
+l_name = gets.chomp
+puts "Hello " + f_name + " " + m_name + " " + l_name
+puts ""
+puts ""
+puts ""
+puts ""
+puts "What is your favorite number?"
+fav_num = gets.chomp.to_i
+better_num = fav_num + 1
+puts better_num.to_s + " is a better and bigger favorite number"
